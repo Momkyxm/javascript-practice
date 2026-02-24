6acff6538c3a86fdb827dc575908538c7f989257
momkyxm <252533557+Momkyxm@users.noreply.github.com>
2026-02-23 18:14:55 -0800

    deleted testcss.css from file permantely not needed for javascript practice at the moment

--- java.js
+++ java.js
@@ -1,7 +1,12 @@
+
 let firstName = "John";
 let lastName = "Doe";
+let a = 10
+console.log(100)
+let b = 7 * a
 
 console.log(firstName);
 console.log(lastName);
+
 
 document.body.innerHTML="<h1>hello,"+firstName+""+lastName+"</h1>";
---
+++ jstest.js
@@ -0,0 +1,6 @@
+//hello
+djhd
+/*hello how
+are
+you 
+*/
