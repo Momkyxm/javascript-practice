4475f118e25922f99e1652c9ff8ebb65c31252fb
momkyxm <252533557+Momkyxm@users.noreply.github.com>
2026-02-23 20:25:06 -0800

    sucessfully run a console.log for js

--- jstest.js
+++ jstest.js
@@ -1,6 +1,13 @@
 //hello
-djhd
 /*hello how
 are
 you 
 */
+let FirstName = "Jane";
+let age = 5*10;
+let gender = "female";
+
+console.log(FirstName);
+console.log(age);
+console.log(gender);
+console.log (`${FirstName} is a ${age}year old ${gender}`);
